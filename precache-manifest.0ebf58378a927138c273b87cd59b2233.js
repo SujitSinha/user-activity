75573574a2c225fc925fe2854c7bc460d2e4fca8
su7jit <sujitsinha.yo@gmail.com>
2020-09-14 07:22:42 +0530

    Updates

---
+++ precache-manifest.0ebf58378a927138c273b87cd59b2233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956804c1c5a6e3a24baf08c01e037308",
+    "url": "/user-activity/index.html"
+  },
+  {
+    "revision": "895b928a6cf74c67e7ca",
+    "url": "/user-activity/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "c5d481195aa98b4acc30",
+    "url": "/user-activity/static/css/main.ea6cc6b9.chunk.css"
+  },
+  {
+    "revision": "895b928a6cf74c67e7ca",
+    "url": "/user-activity/static/js/2.e5c859a0.chunk.js"
+  },
+  {
+    "revision": "e3c54dcb746d9b17bd2a953a46ec38a6",
+    "url": "/user-activity/static/js/2.e5c859a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d481195aa98b4acc30",
+    "url": "/user-activity/static/js/main.438aad97.chunk.js"
+  },
+  {
+    "revision": "dcf0050508e6256ddff4",
+    "url": "/user-activity/static/js/runtime-main.86ca32db.js"
+  }
+]);
